f616c777c713a383cf3fa32c77df5648b9647d51
David Yepez <yepezd@bc.edu>
2019-04-01 12:50:48 -0400

    Initial Commit

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,102 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by David Yépez on 3/31/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import Firebase
+import MapKit
+
+class Spot: NSObject, MKAnnotation {
+    var name: String
+    var address: String
+    var coordinate: CLLocationCoordinate2D
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    var longitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
+    }
+    
+    var dictionary: [String: Any] {
+        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReview": numberOfReviews, "postingUserID": postingUserID]
+    }
+    
+    init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.coordinate = coordinate
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+    convenience override init() {
+        self.init(name: "", address: "", coordinate: CLLocationDegrees, averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    convenience init(dictionary: [String: Any]) {
+        let name = dictionary["name"] as! String? ?? ""
+        let address = dictionary["address"] as! String? ?? ""
+        let latitude = dictionary["latitude"] as! CLLocationDegrees? ?? 0.0
+        let longitude = dictionary["longitude"] as! CLLocationDegrees? ?? 0.0
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let averageRating = dictionary["averageRating"] as! Double? ?? 0.0
+        let numberOfReviews = dictionary["numberOfReviews"] as! Int? ?? 0
+        let postingUserID = dictionary["postingUserID"] as! String? ?? ""
+        self.init(name: name, address: address, coordinate: coordinate, averageRating: averageRating, numberOfReviews: numberOfReviews, postingUserID: postingUserID, documentID: "")
+    }
+    
+    func saveData(completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        guard let postingUserID = (Auth.auth().currentUser?.uid) else {
+            print("***ERROR: could not save data")
+            return completion(false)
+        }
+        self.postingUserID = postingUserID
+        
+        let dataToSave = self.dictionary
+        if self.documentID != "" {
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                if let error = error {
+                    print("***ERROR: updating document \(self.documentID) \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    print("^^^ ERROR: Document updated with ref ID \(ref.documentID)")
+                    completion(true)
+                }
+            }
+        } else {
+            var ref: DocumentReference? = nil
+            ref = db.collection("spots").addDocument(data: dataToSave) {error in
+                if let error = error {
+                    print("***ERROR: creating new document \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    print("^^^ ERROR: New Document created with ref ID \(ref?.documentID ?? "Unknown")")
+                    completion(true)
+                }
+                
+            }
+        }
+    }
+}
---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,38 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by David Yépez on 3/31/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Spots {
+    var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (QuerySnapshot, error) in
+            guard error == nil else {
+                print("***ERROR: adding the snapshot listener \(error!.localizedDescription)")
+                return completed()
+            }
+            self.spotArray = []
+            
+            for document in QuerySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            completed()
+        }
+    }
+    
+    
+}
